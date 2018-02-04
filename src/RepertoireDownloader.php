381a4c06472edbe1fdaba75edbb1e6de83d9cd14
Dorian Villet <gnutix@gmail.com>
2018-02-04 22:51:33 +0100

    Copy YoutubeDl.php in our code.

--- src/RepertoireDownloader.php
+++ src/RepertoireDownloader.php
@@ -10,7 +10,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Finder\Finder;
-use YoutubeDl\YoutubeDl;
+use App\YoutubeDl\YoutubeDl;
 
 final class RepertoireDownloader extends Command
 {
