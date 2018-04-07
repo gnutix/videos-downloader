eedf51910471911fa16087aa0e146e0fcf527af5
Dorian Villet <gnutix@gmail.com>
2018-04-07 17:27:08 +0200

    Allow for fetching Trello 'custom_fields' information + improve the card_properties selector custom syntax.

--- extensions/Trello/Trello.php
+++ extensions/Trello/Trello.php
@@ -8,18 +8,47 @@ use App\Domain\Path;
 use App\Domain\PathPart;
 use App\Domain\Source;
 use App\UI\UserInterface;
+use Doctrine\Common\Collections\ArrayCollection;
 use Stevenmaguire\Services\Trello\Client;
 use Stevenmaguire\Services\Trello\Exceptions\Exception;
+use Symfony\Component\PropertyAccess\Exception\InvalidArgumentException as PropertyPathInvalidArgumentException;
 use Symfony\Component\PropertyAccess\PropertyAccess;
+use Symfony\Component\PropertyAccess\PropertyAccessor;
 use Symfony\Component\Yaml\Yaml;
 
 final class Trello implements Source
 {
+    /**
+     * This regex matches the following custom syntax :
+     *
+     *   1) `[]` allows to loop through all the entries and fetch their nested properties.
+     *
+     *     For example, if [foo] is an array containing N elements having each a key [bar] :
+     *     - `[foo][][bar]` will loop through the N entries of `foo` and get the `bar` nested property for each of them.
+     *
+     *   2) `[property="value"]` does the same as `[]` but filters on one property.
+     *
+     *     For example, if [foo] is an array containing N elements having each a key [bar] and [baz] :
+     *     - `[foo][baz="hello"][bar]` will loop through the N entries of `foo` and get the `bar` nested property for
+     *       each entry matching `entry.baz === "hello"`.
+     *
+     * A selector can only contain ONE custom syntax and it must only occurs ONCE. So the following won't work :
+     *
+     * - `[foo][][bar][][baz]`
+     * - `[foo][][bar][baz="hello"][world]`
+     * - `[foo][bar="hello"][baz="world"][property]`
+     */
+    private const CUSTOM_SYNTAX_EXAMPLES = ['[]', '[property="value"]'];
+    private const CUSTOM_SYNTAX_REGEX = '#^(.*?)(?:\[(?:([a-zA-Z-_]+)="(.*?)")?\])(.*?)$#';
+
     /** @var \App\UI\UserInterface */
     private $ui;
 
     /** @var array */
     private $config;
+
+    /** @var PropertyAccessor */
+    private $propertyAccessor;
 
     /**
      * {@inheritdoc}
@@ -33,6 +62,7 @@ final class Trello implements Source
             (array) Yaml::parseFile(__DIR__.DIRECTORY_SEPARATOR.'config/config.yml'),
             $config
         );
+        $this->propertyAccessor = PropertyAccess::createPropertyAccessor();
 
         foreach (['board_id', 'card_properties'] as $key) {
             if (!isset($this->config[$key]) || empty($this->config[$key])) {
@@ -56,7 +86,13 @@ final class Trello implements Source
             /** @var \Extension\Trello\PhpDoc\TrelloClient $client */
             $client = new Client(['token' => $this->config['api_key']]);
             $trelloLists = $client->getBoardLists($this->config['board_id']);
-            $trelloCards = $client->getBoardCards($this->config['board_id'], ['attachments' => true]);
+            $trelloCards = $client->getBoardCards(
+                $this->config['board_id'],
+                [
+                    'attachments' => true,
+                    'customFieldItems' => true,
+                ]
+            );
         } catch (Exception $e) {
             $this->ui->writeln(
                 sprintf(
@@ -73,42 +109,95 @@ final class Trello implements Source
         $lists = array_combine(array_column($trelloLists, 'id'), $trelloLists);
         $pathPartConfig = $this->config['path_part'];
 
-        $propertyAccessor = PropertyAccess::createPropertyAccessor();
-        $indexId = '[%index%]';
-        $indexIdLength = \strlen($indexId);
-
         // Decode the stdClass object to an array to be used with PropertyAccess
         foreach (json_decode(json_encode($trelloCards), true) as $card) {
-
-            // Get the contents from the card
+            // Get the contents from the card's properties
             $data = [];
-            foreach ((array) $this->config['card_properties'] as $property) {
-                if (false === ($indexIdPosition = strpos($property, '[%index%]'))) {
-                    $data[] = $propertyAccessor->getValue($card, $property);
-                } else {
-                    $rootProperty = substr($property, 0, $indexIdPosition);
-                    $subProperties = substr($property, $indexIdPosition + $indexIdLength);
-
-                    foreach ((array) $propertyAccessor->getValue($card, $rootProperty) as $propertyValues) {
-                        $data[] = $propertyAccessor->getValue($propertyValues, $subProperties);
-                    }
+            foreach ((array) $this->config['card_properties'] as $selector) {
+                foreach ($this->getValuesFromCardMatchingSelector($card, $selector) as $value) {
+                    $data[] = $value;
                 }
             }
 
-            $pathPartConfig['substitutions'] = [
-                // Here we remove any directory separator in the list/song name to avoid unwanted nested folders.
-                // And we remove some characters that are not liked by filesystems too.
-                // Ex: "Songs/AC/DC - Hells Bells" would give "Songs/AC/DC/Hells Bells".
-                '%list_name%' => $this->cleanPath($lists[$card['idList']]->name),
-                '%card_name%' => $this->cleanPath($card['name'])
-            ] + ($pathPartConfig['substitutions'] ?? []);
+            if (!empty($data)) {
+                $listName = $this->propertyAccessor->getValue($lists, '['.$card['idList'].'].name');
+                $cardName = $this->propertyAccessor->getValue($card, '[name]');
 
-            $contents->add(new Content(implode(PHP_EOL, $data), new Path([new PathPart($pathPartConfig)])));
+                $contents->add(
+                    new Content(implode(PHP_EOL, $data), $this->generatePath($pathPartConfig, $listName, $cardName))
+                );
+            }
         }
 
         $this->ui->writeln('<info>Done.</info>');
 
         return $contents;
+    }
+
+    /**
+     * @param string $selector
+     * @param $card
+     *
+     * @return ArrayCollection
+     * @throws \Symfony\Component\PropertyAccess\Exception\UnexpectedTypeException
+     * @throws \Symfony\Component\PropertyAccess\Exception\AccessException
+     * @throws \Symfony\Component\PropertyAccess\Exception\InvalidArgumentException
+     */
+    private function getValuesFromCardMatchingSelector($card, string $selector): ArrayCollection
+    {
+        // If there's no custom syntax, we simply try to fetch the value
+        if (!preg_match(static::CUSTOM_SYNTAX_REGEX, $selector, $matches)) {
+            return new ArrayCollection([$this->propertyAccessor->getValue($card, $selector)]);
+        }
+
+        list(, $rootProperty, $propertyName, $propertyValue, $subProperty) = $matches;
+
+        // Guard against invalid usage of our custom syntax
+        if (preg_match(static::CUSTOM_SYNTAX_REGEX, $subProperty)) {
+            throw new PropertyPathInvalidArgumentException(
+                sprintf(
+                    'The "card_properties" selectors can only contain ONE custom syntax (available: %s).',
+                    '`'.implode('`, `', static::CUSTOM_SYNTAX_EXAMPLES).'`'
+                )
+            );
+        }
+
+        $dataCollection = new ArrayCollection((array) $this->propertyAccessor->getValue($card, $rootProperty));
+
+        // Filtering on property and value
+        if ($propertyName && $propertyValue) {
+            $dataCollection = $dataCollection
+                ->filter(function ($item) use ($propertyName, $propertyValue) {
+                    return $this->propertyAccessor->getValue($item, '['.$propertyName.']') === $propertyValue;
+                });
+        }
+
+        $data = new ArrayCollection();
+        foreach ($dataCollection as $dataCollectionItem) {
+            $data->add($this->propertyAccessor->getValue($dataCollectionItem, $subProperty));
+        }
+
+        return $data;
+    }
+
+    /**
+     * @param array $pathConfig
+     * @param string $listName
+     * @param string $cardName
+     *
+     * @return Path
+     */
+    private function generatePath(array $pathConfig, string $listName, string $cardName): Path
+    {
+        $pathConfig['substitutions'] = [
+                // Here we remove any directory separator in the list/song name to avoid unwanted nested folders.
+                // And we remove some characters that are not liked by filesystems too.
+                // Ex: "AC/DC - Hells Bells" would give "AC/DC/Hells Bells". We prefer "ACDC/Hells Bells".
+                '%list_name%' => $this->cleanPath($listName),
+                '%card_name%' => $this->cleanPath($cardName)
+            ] + ($pathConfig['substitutions'] ?? []);
+
+        return new Path([new PathPart($pathConfig)]);
     }
 
     /**
@@ -118,9 +207,24 @@ final class Trello implements Source
      */
     private function cleanPath(string $string): string
     {
-        $chars = [DIRECTORY_SEPARATOR, '#', '%', '{', '}', '\\', '<', '>', '*', '?', '$', '!', ':', '@'];
-        $sanitizedString = str_replace($chars, '', $string);
+        $chars = [
+            '&' => 'and',
+            DIRECTORY_SEPARATOR => '',
+            '\\' => '',
+            '#' => '',
+            '%' => '',
+            '{' => '',
+            '}' => '',
+            '<' => '',
+            '>' => '',
+            '*' => '',
+            '?' => '',
+            '$' => '',
+            '!' => '',
+            ':' => '',
+            '@' => '',
+        ];
 
-        return (string) str_replace('&', 'and', $sanitizedString);
+        return (string) str_replace(array_keys($chars), array_values($chars), $string);
     }
 }
